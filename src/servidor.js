1a65c0e1151b94eec427b66fd5fa3ddcd39291ac
André Lucas <andrelucca99@gmail.com>
2024-06-21 06:19:49 -0300

    feat: obetendo id do socket

--- src/servidor.js
+++ src/servidor.js
@@ -17,6 +17,4 @@ servidorHttp.listen(PORT, () => console.log(`Servidor escutando na porta ${PORT}
 
 const io = new Server(servidorHttp);
 
-io.on("connection", () => {
-  console.log("Um cliente se conectou!");
-})
+export default io;
